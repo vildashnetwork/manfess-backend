a5f620a960db110cc3d4256e3d2dbfda8e2ecc5e
Li Blissz <liblissz3@gmail.com>
2025-09-06 05:09:32 -0700

    levels and st

--- routes/students/fetchall.js
+++ routes/students/fetchall.js
@@ -4,7 +4,7 @@ const router = e.Router();
 
 // url: https://manfess-backend.onrender.com/api/students
 router.get("/", (req, res) => {
-  const q = "SELECT FirstName, LastName, level,id, Department FROM students where level= 'Level 5'";
+  const q = "SELECT DISTINCT localid, id, FirstName, LastName, level, Department FROM students where level= 'Level 5'";
   db.query(q, (err, data) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json(data);
