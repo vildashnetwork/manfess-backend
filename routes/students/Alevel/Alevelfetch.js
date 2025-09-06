8f6840f1de2b50839a22a9c80a66b0455dde40cc
Li Blissz <liblissz3@gmail.com>
2025-09-06 08:38:26 -0700

    aleve//

--- routes/students/Alevel/Alevelfetch.js
+++ routes/students/Alevel/Alevelfetch.js
@@ -1,19 +1,18 @@
-import e from "express";
+import express from "express";
 import db from "../../../middlewares/db.js";
-const router = e.Router();
+
+const router = express.Router();
 
 // url: https://manfess-backend.onrender.com/api/students/alevel
 router.get("/", (req, res) => {
-  const q = "SELECT DISTINCT localid, id, FirstName, LastName, level, Department FROM studentsalevel where level= 'uppersixth";
+  const q = "SELECT localid, id, FirstName, LastName, level, Department FROM studentsalevel WHERE level = 'uppersixth'";
   db.query(q, (err, data) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json(data);
   });
 });
 
-
 // url: https://manfess-backend.onrender.com/api/students/alevel/all
-
 router.get("/all", (req, res) => {
   const q = "SELECT DISTINCT level FROM studentsalevel";
   db.query(q, (err, data) => {
@@ -22,31 +21,16 @@ router.get("/all", (req, res) => {
   });
 });
 
-
-
-
-
-
-
-
-
-
-//url: https://manfess-backend.onrender.com/api/students/alevel/subjects
-router.get("/subjects", async (req, res)=>{
-    try{
-        db.query("SELECT * FROM subjects where Cycle = 'second_cycle'", (err, results)=>{
-            if(err){
-                console.log(err)
-                return res.status(500).json({error: "Database query error"})
-            }
-            res.status(200).json(results)
-        })
-
+// url: https://manfess-backend.onrender.com/api/students/alevel/subjects
+router.get("/subjects", (req, res) => {
+  const q = "SELECT * FROM subjects WHERE Cycle = 'second_cycle'";
+  db.query(q, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Database query error" });
     }
-    catch(error){
-        console.log(error)
-    }
-})
-
+    res.status(200).json(results);
+  });
+});
 
 export default router;
