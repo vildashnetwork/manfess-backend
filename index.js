4ea7e5d93375e0bfef9ae613fd4c0447bd9f4f15
Li Blissz <liblissz3@gmail.com>
2025-09-07 05:32:14 -0700

    hr

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port http://localhost:${PORT}`);
 })
